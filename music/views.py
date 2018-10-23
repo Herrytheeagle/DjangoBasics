ddd975b1ae886e58bb15b612947e7fd618108bc5
Heritage Samuel F <herrytheeagle@gmail.com>
2018-10-23 19:59:04 +0100

    impvd

--- music/views.py
+++ music/views.py
@@ -1,13 +1,11 @@
 from django.http import Http404
-from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Album
 
 
 def index(request):
     all_albums = Album.objects.all()
-    context = {'all_albums': all_albums}
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
@@ -15,4 +13,5 @@ def detail(request, album_id):
         album = Album.objects.get(pk=album_id)
     except Album.DoesNotExist:
         raise Http404("Album does not exist")
+    return render(request, 'music/index.html', {'all_albums': album})
 
