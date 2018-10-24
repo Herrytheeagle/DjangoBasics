d95c5325cb898bc0ea558c7ceafa99cdf3fe3a7d
Heritage Samuel F <herrytheeagle@gmail.com>
2018-10-24 15:34:20 +0100

    modi

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstdjangowebsite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musiky.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- musiky/wsgi.py
+++ musiky/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for firstdjangowebsite project.
+WSGI config for musiky project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstdjangowebsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musiky.settings')
 
 application = get_wsgi_application()
