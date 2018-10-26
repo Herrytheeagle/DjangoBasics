4785bdc0cd9e140589a256158f8b1a3289bc4f7a
Heritage Samuel F <herrytheeagle@gmail.com>
2018-10-26 21:59:35 +0100

    fm

--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 
 def index(request):
@@ -11,3 +11,12 @@ def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'albums': album})
 
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            
+        })
